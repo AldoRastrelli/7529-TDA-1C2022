6b8d15750a48e1e1c3b8bbb9a6e1b78254f7b985
Aldana Rastrelli <aldanarastrelli@gmail.com>
2022-05-14 21:51:53 -0300

    Funciones auxiliares

---
+++ auxiliares.py
@@ -0,0 +1,70 @@
+from copy import copy
+from cmath import inf
+
+ANTERIOR = 0
+ULTIMA = 1
+
+## Auxiliares ##
+
+def obtener_distancias(grafo, destino):
+    nodos = grafo.nodos
+    distancia_ant = {}
+
+    for n in nodos:
+        if n == destino:
+            distancia_ant[n] = 0
+        else:
+            distancia_ant[n] = inf
+
+    distancia_nueva = {}
+    return [distancia_ant, distancia_nueva]
+
+def no_es_ultima_iteración(i,n):
+    return i != (n-1)
+
+def costo_aun_no_asignado(costo_vert):
+    return costo_vert == inf
+
+def get_diferencias(distancia):
+    diferentes = []
+    for nodo in distancia[ANTERIOR]:
+        if distancia[ANTERIOR][nodo] != distancia[ULTIMA][nodo]:
+            diferentes.append(nodo)
+    return diferentes
+
+def get_diferencias(distancia):
+    diferentes = []
+    for nodo in distancia[ANTERIOR]:
+        if distancia[ANTERIOR][nodo] != distancia[ULTIMA][nodo]:
+            diferentes.append(nodo)
+    return diferentes
+
+def encontrar_ciclo_en(nodos_cambiados, hash_aristas_min):
+    orden = {}
+    visitados = []
+    nodo = nodos_cambiados[0]
+    
+    for i in range(len(hash_aristas_min)+1):
+        if orden.get(nodo) is None:
+            visitados.append(nodo)
+            orden[nodo] = i
+            nodo = hash_aristas_min[nodo]
+            continue
+        visitados.append(nodo)
+        index = orden[nodo]
+        return visitados[index:]
+    
+    return []
+
+def calcular_costo_para(ciclo_negativo, grafo, arista):
+    costo = 0
+    primer_nodo = ciclo_negativo[0]
+    primero = primer_nodo
+    for i in range(1, len(ciclo_negativo)):
+        segundo = arista[primero]
+        costo += grafo[segundo][primero]
+        primero = segundo
+    return costo
+
+def esta_vacio(lista):
+    return len(lista) == 0
